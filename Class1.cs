75ef88df7aa4c7f8b327ec03865ede832bd0f7db
NaveenPulamolu <naveenp992@gmail.com>
2016-12-28 21:03:07 -0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDemo
+{
+    static class Class1
+    {
+        public static void sum()
+        {
+            int a, b, c;
+            a = 20;
+            b = 30;
+            c = a + b;
+            Console.WriteLine("The Sum is {0}", c);
+            
+        }
+    }
+}
